6cb7174d5d4916cbbd41fa98b84c99b7a9de84e0
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-15 09:21:51 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    database.ref().on('value', (snapshot) => {
+      this.setState({
+        data: snapshot.val(),
+        newData: '',
+      });
+    });
+  }
+
+  handleChange(e) {
+    const newData = e.target.value;
+    this.setState({
+      newData,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    database.ref().child('AMAZING NEW DATA').set(this.state.newData);
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,8 +41,13 @@ class App extends Component {
           <h2>Welcome to React and Firebase</h2>
         </div>
         <pre className="App--data">
-          One day, some data from Firebase will go here.
+          { JSON.stringify(this.state.data, null, 2) }
         </pre>
+
+        <form className='App--form' onSubmit={this.handleSubmit}>
+          <input type='text' value={this.state.newData} onChange={this.handleChange} />
+          <input type='submit' />
+        </form>
       </div>
     );
   }
